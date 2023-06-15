159fe46812ae52f9c7fb9427f96a088b5c438ec2
Muhammad Sayem Ijaz <75717799+MuhammadSayemIjaz@users.noreply.github.com>
2023-06-15 23:50:05 +0500

    Create Menu for mobile view

--- Pectice Projects/Landing Page/index.js
+++ Pectice Projects/Landing Page/index.js
@@ -1,1 +1,11 @@
+const menu_section = document.getElementsByClassName('menu_section')[0];
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('close_icon');
 
+menu_section.addEventListener('click', () => {
+     modal.style.display = "flex"
+})
+
+closeModal.addEventListener('click', () => {
+     modal.style.display = "none"
+})
